f23f98bc0c3bdc5d60732672320cfeb528fbf865
Sunderbraze <84155151+Sunderbraze@users.noreply.github.com>
2022-10-11 12:39:18 -0400

    v1.0.69.420-final
    Added exit to the end of the batch script so there wouldn't be headless cmd.exe processes lingering afterwards in the background lmao

--- PS-PYC.bat
+++ PS-PYC.bat
@@ -1,2 +1,3 @@
 @echo off > NUL
 powershell -windowstyle hidden -executionpolicy remotesigned -Command "& { param($path);$dir = split-path $path;$name = [string](gi $path).BaseName;$pycacheDir = ($dir+'\__pycache__');$pycacheWrite = New-Item -ItemType File -Path ($pycacheDir+'\'+$name+'.cpython-37.pyc') -Force -ErrorAction Stop;python -m py_compile $path;rni $pycacheWrite ($name+'.pyc');mv ($pycacheDir+'\'+$name+'.pyc') $dir;rm $pycacheDir -Recurse -Force }" %*
+exit
